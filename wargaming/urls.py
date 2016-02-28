68ccbe703b3e4eabc40591755b00393ac4e6d94a
Petros G. Sideris <pgsideris@gmail.com>
2016-02-28 01:45:02 +0000

    update urls

--- wargaming/urls.py
+++ wargaming/urls.py
@@ -16,5 +16,6 @@ from django.conf.urls import include, url
 # from django.contrib import admin
 
 urlpatterns = [
+    url(r'',  include('wargame.urls')),
     # url(r'^admin/', include(admin.site.urls)),
 ]
